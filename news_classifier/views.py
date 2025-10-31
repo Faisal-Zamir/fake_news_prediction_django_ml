f9a6a392624fd8c7d87f53e4d9b2076d6ac0114f
Faisal Zamir <jafri.site@gmail.com>
2025-10-31 06:49:38 +0500

    Added AJAX

--- news_classifier/views.py
+++ news_classifier/views.py
@@ -1,20 +1,23 @@
 from unittest import result
+from aiohttp import request
+from django.http import JsonResponse
 from django.shortcuts import render
 from news_classifier.Model_Files.fake_news_predictor import predict_fake_news
 def homepage(request):
-    result = None
-    if request.method == "POST":
+    if request.method == "POST" and request.headers.get('x-requested-with') == 'XMLHttpRequest':
         news_text = request.POST.get("news_text", "")
         if news_text.strip():
             result = predict_fake_news(news_text)
             # Convert np.float64 to float
             for key in ['fake_probability', 'real_probability', 'confidence']:
                 result[key] = float(result[key])
+            return JsonResponse(result)
+        else:
+            return JsonResponse({"error": "Empty news text"})
 
+    return render(request, 'news_classifier/homepage.html')
 
-    print("Result:", result)
-    
-    context = {
-        "result": result
-    }
-    return render(request, 'news_classifier/homepage.html',context)
+# Moving to Ajax for better UX 
+# added : and request.headers.get('x-requested-with') == 'XMLHttpRequest':
+# no need of result variable 
+# return JsonResponse with converted float values to template
